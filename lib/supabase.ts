388d1a4452f15c5ab1032763d504947d9e2ce3eb
Atish Bansode <atishbansode2021@gmail.com>
2026-04-12 23:30:24 +0530

    updated

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://itlzrlbfkrvuqhbowdcu.supabase.co'
-const supabaseKey = 'sb_publishable_SosWrra40trxmPAzBkBOEQ_0NpqCSq2'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
